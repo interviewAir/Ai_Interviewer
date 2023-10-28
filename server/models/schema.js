1ba73c140c76a7f9dd6416d0ddbfeee6d5fc2c3e
Jacob Marinas <jacob1marinas@gmail.com>
2023-10-28 04:31:57 -0700

    Setting up backend

---
+++ server/models/schema.js
@@ -0,0 +1,2 @@
+const cockroach = require('cockroach');
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const { Client } = require('pg');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const client = new Client(process.env.DATABASE_URL);
+
+client.connect(); 
+
+client.on('error', (err) => {
+  console.error('Connection error: ${error}');
 });
+
+client.once('open', () => {
+  console.log('Connection successful');
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
